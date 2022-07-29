ef247e91f2705cfa32dab81250f75cc93779f497
Josh Ellice <86125136+jashelish@users.noreply.github.com>
2022-07-29 10:21:54 +0800

    Add files via upload

---
+++ exercise1.m
@@ -0,0 +1,6 @@
+f=imread('C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower.jpg');
+g=imshow(f);
+imwrite(f, 'C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower2.png');
+k=rgb2gray(f);
+imshow(k);
+imwrite(k, 'C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower3.jpg');
